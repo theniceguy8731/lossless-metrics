ecb50d35c575e301092c7ecc26ce623c2eb0ea64
theniceguy8731 <kushagra22263@iiitd.ac.in>
2023-11-18 03:58:41 +0530

    lz78-update

--- src/lz78/lz78.py
+++ src/lz78/lz78.py
@@ -1,61 +1,84 @@
+import varint
+import sys
 def lz78_compress(input_text):
     dictionary = {}
-    next_code = 1
+    next_code = 0
     compressed = bytearray()
-    current_phrase = ""
-
-    for char in input_text:
-        current_phrase += char
-        if current_phrase not in dictionary:
-            dictionary[current_phrase] = next_code
-            compressed.append(dictionary[current_phrase[:-1]])
-            current_phrase = char
+    i = 0
+    while i < len(input_text):
+        if input_text[i] not in dictionary.values():
+            x=0
+            compressed.extend(varint.encode(0))
+            compressed.extend(input_text[i].encode('utf-8'))
             next_code += 1
-
-    if current_phrase in dictionary:
-        compressed.append(dictionary[current_phrase])
+            dictionary[next_code] = input_text[i]
+        else:
+            phrase = input_text[i]
+            while phrase in dictionary.values() and i<len(input_text)-1:
+                i += 1
+                phrase += input_text[i]
+            code=0
+            for x in dictionary.keys():
+                if dictionary[x]==phrase[0:-1]:
+                    code=x
+                    break
+            compressed.extend(varint.encode(code))
+            compressed.extend(phrase[-1].encode('utf-8'))
+            next_code += 1
+            dictionary[next_code] = phrase
+        # print(dictionary.get(next_code),next_code)
+        i += 1
 
     return compressed
 
-
 def lz78_decompress(compressed):
-    dictionary = {0: ""}
-    next_code = 1
-    decompressed = bytearray()
-    current_code = compressed[0]
+    dictionary = {0: b""}
+    next_code = 0
+    i = 0
+    decompressed_text = ""
 
-    for code in compressed[1:]:
-        if code in dictionary:
-            phrase = dictionary[code]
-        elif code == next_code:
-            phrase = dictionary[current_code] + dictionary[current_code][0]
+    while i < len(compressed):
+        code= varint.decode_bytes(compressed[i:])
+        consumed=len(varint.encode(code))
+        i += consumed
+        if code == 0:
+            char = chr(varint.decode_bytes(compressed[i:]))
+            i += 1
+            decompressed_text+=char
+            # print(char)
+            next_code += 1
+            dictionary[next_code] = char
+
         else:
-            raise ValueError("Invalid compressed data")
+            phrase = dictionary[code]
+            char = chr(varint.decode_bytes(compressed[i:]))
+            i += 1
 
-        decompressed.extend(map(ord, phrase))
-        dictionary[next_code] = dictionary[current_code] + phrase[0]
-        next_code += 1
-        current_code = code
+            phrase += char
+            decompressed_text+=phrase
 
-    return decompressed.decode('utf-8')
+            next_code += 1
+            dictionary[next_code] = phrase
+
+    return decompressed_text
 
 
 def main():
-    with open("input.txt", "r", encoding="utf-8") as file:
+    with open("new.txt", "r", encoding="utf-8") as file:
         input_text = file.read()
+    print("Input size:", len(input_text))
 
     compressed_data = lz78_compress(input_text)
     print("Compressed Size:", len(compressed_data))
 
-    with open("compressed_lz78.bin", "wb") as file:
+    with open("../../results/lz78/compressed_lz78.bin", "wb") as file:
         file.write(compressed_data)
-
+    
     decompressed_text = lz78_decompress(compressed_data)
     print("Decompressed Size:", len(decompressed_text))
 
-    with open("decompressed_lz78.txt", "w", encoding="utf-8") as file:
+    with open("../../results/lz78/decompressed_lz78.txt", "w", encoding="utf-8") as file:
         file.write(decompressed_text)
-
 
 if __name__ == "__main__":
     main()
